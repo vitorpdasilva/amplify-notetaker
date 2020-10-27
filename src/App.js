f243deeb47f25f09789e71a2c5fbaa5ec2045ac7
Vitor Boccio <vitorboccio@gmail.com>
2020-10-27 17:10:13 -0400

    ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useState } from 'react';
 import { withAuthenticator } from 'aws-amplify-react';
 
 function App() {
+  const [notes, setNotes] = useState([{ id: 1, note: 'hello' }]);
   return (
-    <div>
-      app
+    <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+      <h1 className="code f2-1">
+        Amplify NoteTaker
+      </h1>
+      <form className="mb3">
+        <input 
+          className="pa2 f4" 
+          placeholder="Write your note"
+        />
+        <button className="pa2 f4" type="submit">
+          Add note
+        </button>
+      </form>
+      <div>
+        {notes.map(({ id, note }) => (
+          <div key={id} className="flex items-center">
+            <li className="list pa1 f3">
+              {note}
+            </li>
+            <button className="bg-transparent bn f4">
+              <span>&times;</span>
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
